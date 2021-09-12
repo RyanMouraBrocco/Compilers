1cfcbe56eab4cb4702924d391296491828389317
RyanMouraBrocco <ryanmourabrocco@gmail.com>
2021-09-12 11:42:03 -0300

    Added all identifiers

--- SimpleLexAnalyzer.c
+++ SimpleLexAnalyzer.c
@@ -10,57 +10,72 @@ typedef enum
     INTEGER_NUMBER,
     ASSIGNMENT,
     WHILE,
-    EOS
+    EOS,
+    AND,
+    BEGIN,
+    BOOLEAN,
+    CHAR,
+    DO,
+    ELSE,
+    END,
+    FALSE,
+    IF,
+    INTEGER,
+    MOD,
+    NOT,
+    OR,
+    PROGRAM,
+    READ,
+    REAL,
+    THEN,
+    TRUE,
+    WRITE
 } TAtom;
+
+typedef struct
+{
+    TAtom atom;
+    int line;
+    int integerAttribute;
+    char idAttribute[15];
+} TInformationAtom;
 
 char *buffer;
 int line = 1;
 
 char *readFile(char *);
-int recognizeId();
-int recognizeNum();
-int getAtom();
-int recognizeAssigment();
-int isWhile(char *);
-char *appendString(char *baseString, char value, int length);
+void recognizeId(TInformationAtom *);
+void recognizeNum(TInformationAtom *);
+TInformationAtom getAtom();
+void recognizeAssigment(TInformationAtom *);
+int compareInsensitiveString(char *, char *, char *);
 
 int main(void)
 {
+    char *bufferToFree = buffer;
     buffer = readFile("input.pas");
     printf("%s\n\n", buffer);
-    int atomo = ERROR;
+    TInformationAtom atom;
 
-    while (atomo != EOS)
+    while (atom.atom != EOS)
     {
-        if (*buffer == ' ')
+        atom = getAtom();
+        if (atom.atom == IDENTIFIER)
+            printf("linha %d: IDENTIFICADOR : %s\n", atom.line, atom.idAttribute);
+        if (atom.atom == INTEGER_NUMBER)
+            printf("linha %d: NUMERO_INTEIRO %d\n", atom.line, atom.integerAttribute);
+        if (atom.atom == ASSIGNMENT)
+            printf("linha %d: ATRIBUIÇÃO\n", atom.line);
+        if (atom.atom == WHILE)
+            printf("linha %d: WILHE : %s\n", atom.line, atom.idAttribute);
+        if (atom.atom == ERROR)
         {
-            buffer++;
-        }
-        else if (*buffer == '\n')
-        {
-            buffer++;
-            line++;
-        }
-        else
-        {
-            atomo = getAtom();
-            if (atomo == IDENTIFIER)
-                printf("linha %d: IDENTIFICADOR\n", line);
-            if (atomo == INTEGER_NUMBER)
-                printf("linha %d: NUMERO_INTEIRO\n", line);
-            if (atomo == ASSIGNMENT)
-                printf("linha %d: ATRIBUIÇÃO\n", line);
-            if (atomo == WHILE)
-                printf("linha %d: WILHE\n", line);
-            if (atomo == ERROR)
-            {
-                printf("linha %d: ERRO\n", line);
-                break;
-            }
+            printf("linha %d: ERRO\n", atom.line);
+            break;
         }
     }
 
-    // free(buffer);
+    free(bufferToFree);
 
     return 0;
 }
@@ -89,45 +104,56 @@ char *readFile(char *fileName)
     return readBuffer;
 }
 
-int getAtom()
+TInformationAtom getAtom()
 {
-    int atomo = ERROR;
+    TInformationAtom atom;
+    atom.atom = ERROR;
 
-    if (*buffer == ':')
+    if (*buffer == ' ')
     {
-        atomo = recognizeAssigment();
+        buffer++;
+    }
+    else if (*buffer == '\n')
+    {
+        buffer++;
+        line++;
+    }
+
+    atom.line = line;
+    if (*buffer == '\x0')
+    {
+        atom.atom = EOS;
+        atom.line = line;
+    }
+    else if (*buffer == ':')
+    {
+        recognizeAssigment(&atom);
     }
     else if (isalpha(*buffer))
     {
-        atomo = recognizeId();
+        recognizeId(&atom);
     }
     else if (isdigit(*buffer))
     {
-        atomo = recognizeNum();
-    }
-    else if (*buffer == NULL)
-    {
-        atomo = EOS;
+        recognizeNum(&atom);
     }
 
-    return atomo;
+    return atom;
 }
 
-int recognizeId()
+void recognizeId(TInformationAtom *atom)
 {
-    char *word = NULL;
+    char *initId = buffer;
     int length = 0;
 r0:
     if (isalpha(*buffer))
     {
-        word = appendString(word, *buffer, length++);
         buffer++;
         goto r1;
     }
 r1:
-    if (isalpha(*buffer) || isdigit(*buffer))
+    if (isalpha(*buffer) || isdigit(*buffer) || *buffer == '_')
     {
-        word = appendString(word, *buffer, length++);
         buffer++;
         goto r1;
     }
@@ -136,16 +162,81 @@ r1:
         goto r2;
     }
 r2:
-    if (isWhile(word))
+    length = buffer - initId;
+    if (length <= 16)
     {
-        return WHILE;
+        strncpy(atom->idAttribute, initId, length);
+        atom->idAttribute[length] = '\x0';
+        if (compareInsensitiveString(atom->idAttribute, "WHILE", "while") == 0)
+            atom->atom = WHILE;
+        else if (compareInsensitiveString(atom->idAttribute, "AND", "and") == 0)
+            atom->atom = AND;
+        else if (compareInsensitiveString(atom->idAttribute, "BEGIN", "begin") == 0)
+            atom->atom = BEGIN;
+        else if (compareInsensitiveString(atom->idAttribute, "BOOLEAN", "boolean") == 0)
+            atom->atom = BOOLEAN;
+        else if (compareInsensitiveString(atom->idAttribute, "CHAR", "char") == 0)
+            atom->atom = CHAR;
+        else if (compareInsensitiveString(atom->idAttribute, "DO", "do") == 0)
+            atom->atom = DO;
+        else if (compareInsensitiveString(atom->idAttribute, "ELSE", "else") == 0)
+            atom->atom = ELSE;
+        else if (compareInsensitiveString(atom->idAttribute, "END", "end") == 0)
+            atom->atom = END;
+        else if (compareInsensitiveString(atom->idAttribute, "FALSE", "false") == 0)
+            atom->atom = FALSE;
+        else if (compareInsensitiveString(atom->idAttribute, "IF", "if") == 0)
+            atom->atom = IF;
+        else if (compareInsensitiveString(atom->idAttribute, "INTEGER", "integer") == 0)
+            atom->atom = INTEGER;
+        else if (compareInsensitiveString(atom->idAttribute, "MOD", "mod") == 0)
+            atom->atom = MOD;
+        else if (compareInsensitiveString(atom->idAttribute, "NOT", "not") == 0)
+            atom->atom = NOT;
+        else if (compareInsensitiveString(atom->idAttribute, "OR", "or") == 0)
+            atom->atom = OR;
+        else if (compareInsensitiveString(atom->idAttribute, "PROGRAM", "program") == 0)
+            atom->atom = PROGRAM;
+        else if (compareInsensitiveString(atom->idAttribute, "READ", "read") == 0)
+            atom->atom = READ;
+        else if (compareInsensitiveString(atom->idAttribute, "REAL", "real") == 0)
+            atom->atom = REAL;
+        else if (compareInsensitiveString(atom->idAttribute, "THEN", "then") == 0)
+            atom->atom = THEN;
+        else if (compareInsensitiveString(atom->idAttribute, "TRUE", "true") == 0)
+            atom->atom = TRUE;
+        else if (compareInsensitiveString(atom->idAttribute, "WRITE", "write") == 0)
+            atom->atom = WRITE;
+        else
+            atom->atom = IDENTIFIER;
     }
-
-    return IDENTIFIER;
+    else
+    {
+        atom->atom = ERROR;
+    }
 }
 
-int recognizeNum()
+int compareInsensitiveString(char *valueToCompare, char *upperCaseValue, char *lowerCaseValue)
 {
+    int toCompareLen = strlen(valueToCompare);
+    int len = strlen(upperCaseValue);
+    if (len != toCompareLen)
+        return 1;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (valueToCompare[i] != upperCaseValue[i] && valueToCompare[i] != lowerCaseValue[i])
+            return 1;
+    }
+
+    return 0;
+}
+
+void recognizeNum(TInformationAtom *atom)
+{
+    char *initNum = buffer;
+    int length = 0;
+    char stringNum[20];
 r0:
     if (isdigit(*buffer))
     {
@@ -169,12 +260,17 @@ r1:
         goto r2;
     }
 r2:
-    return INTEGER_NUMBER;
+
+    atom->atom = INTEGER_NUMBER;
+    length = buffer - initNum;
+    strncpy(stringNum, initNum, length);
+    atom->integerAttribute = atoi(stringNum);
+    return;
 r3:
-    return ERROR;
+    atom->atom = ERROR;
 }
 
-int recognizeAssigment()
+void recognizeAssigment(TInformationAtom *atom)
 {
 r0:
     if (*buffer == ':')
@@ -186,56 +282,10 @@ r1:
     if (*buffer == '=')
     {
         buffer++;
-        return ASSIGNMENT;
-    }
-
-    return ERROR;
-}
-
-int isWhile(char *localBuffer)
-{
-    char whileUpperName[] = "WHILE";
-    char whileLowerName[] = "while";
-    int index = 0;
-    while (isalpha(*localBuffer) || isdigit(*localBuffer))
-    {
-        if (index > 4)
-        {
-            return 0;
-        }
-
-        if (*localBuffer == whileUpperName[index] || *localBuffer == whileLowerName[index])
-        {
-            localBuffer++;
-            index++;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-
-    return 1;
-}
-
-char *appendString(char *baseString, char value, int length)
-{
-    if (baseString == NULL)
-    {
-        char *newString = malloc(1);
-        newString[0] = value;
-        return newString;
+        atom->atom = ASSIGNMENT;
     }
     else
     {
-        char *newString = malloc((length + 2) * sizeof(char));
-        for (int i = 0; i < length; i++)
-        {
-            newString[i] = baseString[i];
-        }
-
-        newString[length] = value;
-        newString[length + 1] = '\0';
-        return newString;
+        atom->atom = ERROR;
     }
 }
